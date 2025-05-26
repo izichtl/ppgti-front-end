31a42144df06f46b7b72c72a6a46d656557c8c2f
Ivan Zichtl <izichtl@gmail.com>
2025-05-26 20:30:34 -0300

    develop(develop): updated dashboard nav icons

--- src/pages/layout/inner-layout/r-components/MenuContent.tsx
+++ src/pages/layout/inner-layout/r-components/MenuContent.tsx
@@ -1,22 +1,18 @@
-import * as React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Stack from '@mui/material/Stack';
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import AnalyticsRoundedIcon from '@mui/icons-material/AnalyticsRounded';
-import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
-import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
-import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
-import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+import ArticleIcon from '@mui/icons-material/Article';
+import RssFeedIcon from '@mui/icons-material/RssFeed';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
 
 const mainListItems = [
-  { href: '/process', text: 'Inscrever', icon: <AnalyticsRoundedIcon /> },
-  { href: '/documents', text: 'Documentos', icon: <HomeRoundedIcon /> },
-  { href: '/blog', text: 'Notícias', icon: <AssignmentRoundedIcon /> },
+  { href: '/process', text: 'Inscrever', icon: <AppRegistrationIcon /> },
+  { href: '/documents', text: 'Documentos', icon: <ArticleIcon /> },
+  { href: '/blog', text: 'Notícias', icon: <RssFeedIcon /> },
 ];
 
 const secondaryListItems = [
